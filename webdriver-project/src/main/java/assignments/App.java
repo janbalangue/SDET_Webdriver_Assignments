353a75d19313987eb33c937608e505f32f532c80
Jan_Balangue <jan.a.balangue@gmail.com>
2022-07-16 14:19:50 -0500

    Change comment

--- webdriver-project/src/main/java/assignments/App.java
+++ webdriver-project/src/main/java/assignments/App.java
@@ -15,7 +15,7 @@ public class App {
 	
 	public static void setProperties() {
 		
-		// to run locally, replace the filepaths with local values
+		// to run, replace the filepaths with local values
 		System.setProperty("webdriver.chrome.driver","/opt/homebrew/Caskroom/chromedriver/103.0.5060.53/chromedriver");
 		System.setProperty("webdriver.gecko.driver", "/opt/homebrew/Cellar/geckodriver/0.31.0/bin/geckodriver");
 	}
