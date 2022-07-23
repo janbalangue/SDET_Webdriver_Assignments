f52d7a251031342f17e211b46afbfd7f1343cfde
Jan_Balangue <jan.a.balangue@gmail.com>
2022-07-22 22:10:44 -0500

    remove commented lines

--- webdriver-project/src/main/java/assignments/App0717.java
+++ webdriver-project/src/main/java/assignments/App0717.java
@@ -7,9 +7,6 @@ public class App0717 {
 
 	public static void main(String[] args) {
 		new Properties().setProperties();
-//		Realtor.com now suspects my bot
-//		new Realtor().runTest(new ChromeDriver());
-//		new Realtor().runTest(new FirefoxDriver());
 		new SauceDemo().runTest(new ChromeDriver());
 		new SauceDemo().runTest(new FirefoxDriver());
 	}
